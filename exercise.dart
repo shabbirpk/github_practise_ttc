12626606b48f93b4e80313437e660dbdba837781
SHABBIR AHMED <shabbirahmedbd40@gmail.com>
2021-11-01 10:58:57 +0600

    first ex

--- exercise.dart
+++ exercise.dart
@@ -1,3 +1,16 @@
-void main() {
-  print("hello");
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Git Exercise'),
+        ),
+      ),
+    );
+  }
 }
